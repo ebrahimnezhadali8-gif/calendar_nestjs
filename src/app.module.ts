78be79c135522fe3fd32e5c6cf8a1e2474cebaa2
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2026-01-06 12:40:03 -0800

    Create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (ConfigService : ConfigService) =>({
+        type : 'postgres',
+        host : ConfigService.get('DB_HOST'),
+        port : +ConfigService.get('DB_PORT'),
+        username : ConfigService.get('DB_USERNAME'),
+        password : ConfigService.get('DB_PASSWORD'),
+        database : ConfigService.get('DB_NAME'),
+        entities : [join(process.cwd() , 'dist/**/*.entity.js')],
+        // do not use synchronize : true in real project 
+        synchronize: true,
+      }),
+    }),
+    EventsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
