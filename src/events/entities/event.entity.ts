8792d1f7f9a4729c7e5e7a22d4e7f7fa13c4b3c3
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2026-01-06 12:40:20 -0800

    Create event.entity.ts

---
+++ src/events/entities/event.entity.ts
@@ -0,0 +1,31 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('events')
+export class Event {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar' , length: 100})
+    @Index()
+    title: string;
+
+    @Column({ type: 'text' , nullable: true })
+    description?: string;
+
+    @Column({ type: 'timestamp' })
+    @Index()
+    start: Date;
+
+    @Column({ type: 'timestamp' })
+    @Index()
+    end: Date;
+
+    @CreateDateColumn()
+    createAt: Date;
+
+    @UpdateDateColumn()
+    updateAt: Date;
+
+    @DeleteDateColumn({ nullable: true })
+    deletedAt?: Date;
+}
