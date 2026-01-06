3d80abc3d47d52b88d5c486d873517dbb7848aec
AliDevHub <237058951+ebrahimnezhadali8-gif@users.noreply.github.com>
2026-01-06 12:40:27 -0800

    Create events.controller.ts

---
+++ src/events/events.controller.ts
@@ -0,0 +1,40 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { EventsService } from './events.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { UpdateEventDto } from './dto/update-event.dto';
+import { DeleteEventDto } from './dto/delete-event.dto';
+
+@Controller('events')
+export class EventsController {
+  constructor(private readonly eventsService: EventsService) {}
+
+  @Post()
+  add(@Body() createEventDto: CreateEventDto) {
+    return this.eventsService.addEvent(createEventDto);
+  }
+
+  @Patch(':id')
+  edit(@Param('id') id: string, @Body() updateEventDto: UpdateEventDto) {
+    return this.eventsService.UpdateEvent(+id, updateEventDto);
+  }
+
+  @Delete()
+  remove(@Query() dto: DeleteEventDto) {
+    return this.eventsService.removeEvent(dto.start);
+  }
+
+  @Get('day')
+  listDay() {
+    return this.eventsService.findAll();
+  }
+
+  @Get(':id')
+  listWeek(@Param('id') id: string) {
+    return this.eventsService.findOne(+id);
+  }
+
+  @Get(':id')
+  listMonth(@Param('id') id: string) {
+    return this.eventsService.findOne(+id);
+  }
+}
